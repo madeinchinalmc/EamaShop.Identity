c29c6d7d227f54240147fb1e72706733c8a8f1d4
亚锋 吴 <wuyafeng909099000@qq.com>
2017-12-27 22:42:09 +0800

    enable docker support

---
+++ test/EamaShop.Respository.Tests/RespositoryTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace EamaShop.Respository.Tests
+{
+    public class RespositoryTests
+    {
+        [Fact]
+        public void Do_Test()
+        {
+
+        }
+    }
+}
