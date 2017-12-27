fb2df94fc5cb0c265ce8284221d6030c7f641fba
亚锋 吴 <wuyafeng909099000@qq.com>
2017-12-27 20:49:23 +0800

    push

---
+++ test/EamaShop.Service.Test/Base/BaseTests.cs
@@ -0,0 +1,32 @@
+﻿using EamaShop.Identity.API;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EamaShop.Service.Test.Base
+{
+    internal class BaseTests
+    {
+        protected IServiceProvider RootProvider { get; }
+        public BaseTests()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var env = new HostingEnvironment()
+            {
+                ApplicationName = "EamaShop.Identity.API",
+                EnvironmentName = "Development"
+            };
+            var startuper = new Startup(configuration, env);
+
+            var service = new ServiceCollection();
+            startuper.ConfigureServices(service);
+
+            RootProvider = service.BuildServiceProvider();
+        }
+    }
+}
---
+++ test/EamaShop.Service.Test/UserTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace EamaShop.Service.Test
+{
+    public class UserTest
+    {
+
+        [Fact]
+        public void Regiter_User_Test()
+        {
+
+        }
+    }
+}
