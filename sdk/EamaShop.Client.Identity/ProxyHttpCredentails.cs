6bc6bce7dee957eb7d6a020a8bd7ffd7675e5ca5
亚锋 吴 <wuyafeng909099000@qq.com>
2017-12-28 21:36:31 +0800

    Add Identity Service Accessing Client

---
+++ sdk/EamaShop.Client.Identity/ProxyHttpCredentails.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+namespace EamaShop.Client.Identity
+{
+    public class ProxyHttpCredentails : ServiceClientCredentials
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+        public ProxyHttpCredentails(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+        public override void InitializeServiceClient<T>(ServiceClient<T> client)
+        {
+            base.InitializeServiceClient(client);
+        }
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var headers = _httpContextAccessor?.HttpContext?.Request?.Headers;
+
+            if (headers == null)
+            {
+                return base.ProcessHttpRequestAsync(request, cancellationToken);
+            }
+
+            var authorization = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authorization[1]);
+
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ sdk/EamaShop.Client.Identity/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using EamaShop.Client.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Provides method for add <see cref="IIdentityClient"/>
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIdentityClient(this IServiceCollection services, Uri baseUri)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+            services.TryAddSingleton<IIdentityClient>(sp =>
+            {
+                var httpAccessor = sp.GetRequiredService<IHttpContextAccessor>();
+                return new IdentityClient(baseUri, new ProxyHttpCredentails(httpAccessor));
+            });
+
+            return services;
+        }
+    }
+}
